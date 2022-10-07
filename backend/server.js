4eaabc5abdc89990131d9d4844bf587d95f82de4
Ede Success <edesuccess54@gmail.com>
2022-10-07 15:26:12 +0100

    working on user controller functions

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,33 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const errorHandler = require('./middleWare/errorMiddleWare')
+const cookieParser = require('cookie-parser')
+
+const userRoute = require('./routes/userRoute');
 
 const app = express();
-const MONGO_URI = process.env.MONGO_URI
-const PORT = process.env.PORT || 6000;
 
+// middlewares
+app.use(express.json());
+app.use(cookieParser( ))
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// routes middleware
+app.use("/api/users", userRoute)
+
+// routes
+app.get("/", (req, res) => {
+    res.send("Home page");
+})
+
+// error middleware
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 6000;
 // connect to mongoDB and start server
-mongoose.connect(MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     app.listen(PORT, () => {
         console.log(`server running on port ${PORT}`);
