c524b793bc19288e35686afebe9f3d65f12fb327
Ede Success <edesuccess54@gmail.com>
2022-10-21 17:36:33 +0100

    made changes

--- backend/middleWare/authMiddleWare.js
+++ backend/middleWare/authMiddleWare.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 
 const protect = asyncHandler(async(req, res, next) => {
     try {
-        const token = req.cookie.token;
+        const token = req.cookies.token;
 
         console.log("here is the token: ", token)
 
