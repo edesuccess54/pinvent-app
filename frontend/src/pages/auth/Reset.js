98302e20326e5016193c283b63a892b5da87beb7
Ede Success <edesuccess54@gmail.com>
2022-11-11 00:11:30 +0100

    cereated reset comp

--- frontend/src/pages/auth/Reset.js
+++ frontend/src/pages/auth/Reset.js
@@ -1,6 +1,47 @@
+import styles from './auth.module.scss'
+import { MdPassword } from 'react-icons/md'
+import Card from '../../components/card/Card'
+import { Link } from 'react-router-dom'
+
 const Reset = () => {
   return (
-    <div>Reset</div>
+    <div className= {`container ${styles.auth}`}>
+      <Card>
+        <div className={styles.form}>
+            <div className="--flex-center">
+                <MdPassword size={35} color="#999" />
+            </div>
+            <h2>Reset Password</h2>
+
+            <form>
+                <input 
+                type="password" 
+                placeholder='New password'
+                required
+                name="password" 
+                />
+
+<               input 
+                type="password" 
+                placeholder='Confirm New password'
+                required
+                name="password" 
+                />
+
+                <button type='submit' className='--btn --btn-primary --btn-block'>Reset Password</button>
+
+                <div className={styles.links}>
+                    <p>
+                        <Link to="/">- Home</Link>
+                    </p>
+                    <p>
+                        <Link to="/login">- Register</Link>
+                    </p>
+                </div>
+            </form>
+        </div>
+      </Card>
+    </div>
   )
 }
 
