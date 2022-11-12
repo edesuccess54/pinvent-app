40653765f62363341a2753746cdb2708bed7b1cd
Ede Success <edesuccess54@gmail.com>
2022-11-12 22:51:21 +0100

    routing

---
+++ frontend/src/components/sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import './Sidebar.css'
+
+const Sidebar = ({children}) => {
+  return (
+    <div className="layout">
+      <div className="sidebar">
+        <h2>Sidebar</h2>
+      </div>
+      <main>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Sidebar
