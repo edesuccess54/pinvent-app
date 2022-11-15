54310ff7d38b396c1dd0091326210011d13e17a8
Ede Success <edesuccess54@gmail.com>
2022-11-15 15:17:11 +0100

    changes

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <div className='--pad header'>
       <div className="--flex-between">
         <h3>
-            <span className='--fw-thin'>Welcome,</span>
+            <span className='--fw-thin'>Welcome, </span>
             <span className='--color-danger'>Edesuccess</span>
         </h3>
         <button className="--btn --btn-danger">
