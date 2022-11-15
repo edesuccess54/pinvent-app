22cf272c46b07f85fb670ef8f4ebb79ac84b890d
Ede Success <edesuccess54@gmail.com>
2022-11-15 15:40:04 +0100

    added logo and hamburger

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -1,10 +1,23 @@
 import './Sidebar.scss'
+import { HiMenuAlt3 } from 'react-icons/hi'
+import { RiProductHuntLine } from 'react-icons/ri'
+import menu from '../../data/sidebar'
+import SidebarItem from './SidebarItem'
 
 const Sidebar = ({children}) => {
   return (
     <div className="layout">
       <div className="sidebar">
-        <h2>Sidebar</h2>
+        
+        <div className="top_section">
+          <div className="logo">
+            <RiProductHuntLine  size={35} style={{cursor: "pointer"}}/>
+          </div>
+          <div className="bars">
+            <HiMenuAlt3 />
+          </div>
+        </div>
+
       </div>
       <main>
         {children}
